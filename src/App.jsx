4f3a18fbd0a0951d8fd1113717ce8d1449ce2c8a
Agustian898 <blimp-lunar-turban@duck.com>
2026-02-18 16:34:43 +0700

    Create main component file for Timelapse Architect

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TimelapseArchitect = () => {
+    return (
+        <div>
+            <h1>Timelapse Architect</h1>
+            <p>This is the main component of the Timelapse Architect application.</p>
+        </div>
+    );
+};
+
+export default TimelapseArchitect;
